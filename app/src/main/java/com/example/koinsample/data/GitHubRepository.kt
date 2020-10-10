eec9aecb1c68765f24a3e01fca43a3dd964fb4d1
tanifumiya <fumiyadeveloper@gmail.com>
2020-10-11 04:47:19 +0900

    ignore修正

--- app/src/main/java/com/example/koinsample/data/GitHubRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.koinsample.data
-
-import com.example.koinsample.data.entity.Repo
-
-interface GitHubRepository {
-    suspend fun getRepositories(userName: String): List<Repo>
-}
--- app/src/main/java/com/example/koinsample/data/entity/License.kt
+++
@@ -1,8 +0,0 @@
-package com.example.koinsample.data.entity
-
-data class License(
-    val name: String,
-    val key: String,
-    val spdx_id: String,
-    val url: String,
-)
